cae767799b2fdc7b546f19d1f9e24daeef2f0c03
leonardobrito <f.leobrito@gmail.com>
2023-01-29 00:12:05 -0300

    :white_check_mark: Add pass tests: 3

--- test/todo_app_web/controllers/todo_item_controller_test.exs
+++ test/todo_app_web/controllers/todo_item_controller_test.exs
@@ -4,7 +4,7 @@ defmodule TodoAppWeb.TodoItemControllerTest do
   describe "index" do
     test "list all to do items", %{conn: conn} do
       conn = get(conn, Routes.todo_item_path(conn, :index))
-      assert html_response(conn, 200)
+      assert html_response(conn, 200) =~ "Listing Todo Items."
     end
   end
 end
